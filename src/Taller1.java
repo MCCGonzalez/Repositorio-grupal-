41255fb579ea5b0eac610ced6be1803bec12b48c
Jose <j.huircaleo01@ufromail.cl>
2022-03-23 09:56:24 -0300

    taller1 23

--- src/Taller1.java
+++ src/Taller1.java
@@ -1,18 +1,23 @@
 public class Taller1 {
     public static void main(String[] args) {
-        int largo = (int) (Math.random()*20);
-        LargoArr(largo);
+        LargoArr();
+        MostarArr(LargoArr());
     }
 
-    private static int[] LargoArr(int largo) {
+    private static void MostarArr(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.print("[" + arreglo[i] + "]");
 
-        int[] arreglo = new int[largo];
-        for(int i=0;i< arreglo.length;i++){
-            System.out.print("["+arreglo[i]+"]");
+            System.out.println(" ");
         }
-        System.out.println(" ");
+    }
 
+    private static int[] LargoArr() {
+        int largo = (int) (Math.random() * 20 + 1);
+        int[] arreglo = new int[largo];
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] = (int) (Math.random() * 14 + 1);
+        }
         return arreglo;
     }
 }
-
