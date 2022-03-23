f7956078912034b55b3306f760afaa1129f10928
Jose <j.huircaleo01@ufromail.cl>
2022-03-22 21:48:33 -0300

    pruebas2

---
+++ src/Prueba.java
@@ -0,0 +1,9 @@
+public class Prueba {
+    public static void main(String[] args){
+        LLnear();
+    }
+
+    private static void LLnear() {
+        System.out.println("Hola mundo");
+    }
+}
